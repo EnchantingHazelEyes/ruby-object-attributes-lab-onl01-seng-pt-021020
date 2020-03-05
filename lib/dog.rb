355935068c62ba2ff90cf374338d0570446d860e
Samantha Barros <samantha.barros95@gmail.com>
2020-03-05 16:57:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,8 +2,10 @@ class Dog
   
   def name=(dog_name)
     @doggy_name = dog_name
-     dog_name.instance_variable_get(@doggy_name)
+     #dog_name.instance_variable_get(@doggy_name)
   end
+       dog_name.instance_variable_get(@doggy_name)
+
   
   def name
      #@doggy_name
